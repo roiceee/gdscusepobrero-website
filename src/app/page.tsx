ef9120be7fd9362842696feabe972f66f811f85c
jroiceee <jroicealdeza@gmail.com>
2023-09-16 01:18:13 +0800

    add blur placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex h-screen w-screen items-center justify-center">
       <div>
-        <Image src={gdsc} alt="GDSC Icon" height={300}/>
+        <Image src={gdsc} alt="GDSC Icon" height={300} placeholder="blur"/>
         <h1 className=" text-4xl text-center">
           Currently on planning phase...
         </h1>
