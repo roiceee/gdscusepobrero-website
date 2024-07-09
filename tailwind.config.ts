0bdf6b239b9c28a70592a37b6095d975b653b375
John Roice Aldeza <jroicealdeza@gmail.com>
2024-07-09 21:34:43 +0800

    feat: Add lazy loading for images and update typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography"
 
 const withMT = require("@material-tailwind/react/utils/withMT");
 
@@ -9,22 +10,28 @@ const config: Config = withMT({
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'red': '#ea4335',
-      'yellow': '#f9ab00',
-      'green': '#34a853',
-      'blue': '#4285f4',
-      'red-mute': '#f28b82',
-      'yellow-mute': '#fde293',
-      'green-mute': '#81c995',
-      'blue-mute': '#8ab4f8',
-      'gray': '#5f6368',
-      'light-gray': "#f1f3f4",
-      'maroon': "#9a0000",
-      'black': "#2C384A",
+    fontFamily: {
+      sans: ["Google Sans", "sans-serif"],
+      serif: ["Google Sans", "serif"],
+      body: ["Google Sans", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        red: "#ea4335",
+        yellow: "#f9ab00",
+        green: "#34a853",
+        blue: "#4285f4",
+        "red-mute": "#f28b82",
+        "yellow-mute": "#fde293",
+        "green-mute": "#81c995",
+        "blue-mute": "#8ab4f8",
+        gray: "#5f6368",
+        "light-gray": "#f1f3f4",
+        maroon: "#9a0000",
+        black: "#2C384A",
+      },
+    },
   },
-  plugins: [],
+  plugins: [typography],
 });
 export default config;
