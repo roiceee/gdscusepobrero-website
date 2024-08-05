7c54c327ad1a729de75b8c880fe462ef3e4b22bb
John Roice Aldeza <jroicealdeza@gmail.com>
2024-08-05 15:17:31 +0800

    feat: Add new images to the gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -17,6 +17,11 @@ import imageEleven from "/public/images/events/11.png";
 import imageTwelve from "/public/images/events/12.png";
 import imageThirteen from "/public/images/events/13.png";
 import imageFourteen from "/public/images/events/14.png";
+import imageFifteen from "/public/images/events/15.png";
+import imageSixteen from "/public/images/events/16.png";
+import imageSeventeen from "/public/images/events/17.png";
+import imageEighteen from "/public/images/events/18.png";
+
 
 export default function Gallery() {
   const data = [
@@ -54,11 +59,23 @@ export default function Gallery() {
       imgelink: imageTwelve,
     },
     {
+      imgelink: imageFifteen
+    },
+    {
+      imgelink: imageSixteen
+    },
+    {
+      imgelink: imageSeventeen
+    },
+    {
+      imgelink: imageEighteen
+    },
+    {
       imgelink: imageThirteen,
     },
     {
       imgelink: imageFourteen
-    }
+    },
   ];
 
   return (
