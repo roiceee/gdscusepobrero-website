f73a9dea5296d98c5d5903b8e5c4e1944c9fdd2e
Alexander Villasis <alexandervaugn@gmail.com>
2024-08-06 20:16:01 +0800

    Contact Page Finish

--- src/components/client-wrapper.tsx
+++ src/components/client-wrapper.tsx
@@ -1,4 +1,7 @@
 "use client"
 import { ThemeProvider, Button } from "@material-tailwind/react";
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import EmailIcon from '@mui/icons-material/Email';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export { ThemeProvider, Button };
+export { ThemeProvider, Button, PhoneInTalkIcon, EmailIcon, LocationOnIcon };
