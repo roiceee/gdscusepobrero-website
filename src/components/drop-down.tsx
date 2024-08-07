ba782247908b88144a5d23b07294f344b2e87f75
John Roice Aldeza <jroicealdeza@gmail.com>
2024-08-08 00:06:59 +0800

    feat: Update drop-down component with dynamic items and improved styling

--- src/components/drop-down.tsx
+++ src/components/drop-down.tsx
@@ -3,6 +3,17 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
 
+const dropdownItems = [
+  {
+    name: "2023-2024",
+    url: "/team/2023-2024",
+  },
+  {
+    name: "2022-2023",
+    url: "/team/2022-2023",
+  },
+];
+
 function DropDown() {
   const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
@@ -30,7 +41,7 @@ function DropDown() {
   }, []);
 
   return (
-    <main className="dropdown relative">
+    <main className="dropdown relative w-fit mx-auto">
       <button
         data-ripple-light="true"
         data-popover-target="menu"
@@ -47,12 +58,17 @@ function DropDown() {
           data-popover-placement="bottom"
           className="absolute left-1/2 -translate-x-1/2 top-full z-10 mt-2 min-w-[180px] overflow-auto rounded-md border border-blue-gray-50 bg-white p-3 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none"
         >
-          <li
-            role="menuitem"
-            className="block w-full cursor-pointer select-none rounded-md px-3 pt-[9px] pb-2 text-start leading-tight transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
-          >
-            <Link href={"/team/2023-2024"}>2023-2024</Link>
-          </li>
+          {dropdownItems.map((item) => (
+            <Link href={item.url} key={item.name + "-dropdown-item"}>
+              <li
+                onClick={() => setIsOpen(false)}
+                role="menuitem"
+                className="block text-lg w-full cursor-pointer select-none rounded-md px-3 pt-[9px] pb-2 text-start leading-tight transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+              >
+                {item.name}
+              </li>
+            </Link>
+          ))}
         </ul>
       )}
     </main>
