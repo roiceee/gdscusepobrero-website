75f0919dbf7e2be68a49dc8b3c08fa1c6c952663
John Roice Aldeza <jroicealdeza@gmail.com>
2024-08-13 18:21:20 +0800

    refactor: Add close button to AlertDismissible component

--- src/components/alert-dismissible.tsx
+++ src/components/alert-dismissible.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Link from "next/link";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 
 interface Props {
   linkUrl?: string;
@@ -9,18 +11,34 @@ interface Props {
 }
 
 export function AlertDismissible({ linkUrl, linkMessage, message }: Props) {
+  const [show, setShow] = useState(true);
+
   return (
     <>
-      <div className="bg-maroon text-white p-4 rounded-none justify-center text-sm sm:text-md md:text-lg">
-        <div className="text-center mx-auto mr-0">
-          {message}
-          {linkUrl && (
-            <Link href={linkUrl} className="underline ml-2 font-bold" target="_blank">
-              {linkMessage}
-            </Link>
-          )}
+      {show && (
+        <div className="bg-maroon text-white p-4 rounded-none flex items-center justify-center text-sm sm:text-md md:text-lg">
+          <div>
+            {message}
+            {linkUrl && (
+              <Link
+                href={linkUrl}
+                className="underline ml-2 font-bold"
+                target="_blank"
+              >
+                {linkMessage}
+              </Link>
+            )}
+          </div>
+          <div>
+            <span
+              className="w-fit ml-4 hover:opacity-90"
+              onClick={() => setShow(false)}
+            >
+              <CloseIcon />
+            </span>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
